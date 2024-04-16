ecff27dd74a24b80a2148c0a79fb197b2333c2ae
Tanya Dewland <tanya.dewland@gmail.com>
2024-04-16 11:08:01 +0200

    House price calculator

---
+++ javascript/javascript1/week1/house_price.js
@@ -0,0 +1,18 @@
+function housePrice(width,depth,height, gardenSizeInM2){
+    let volumeInMeters = width*depth*height;
+    return volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+
+}
+if (housePrice(8,10,10,100)> 2500000){
+    console.log('Peter is paying too much.');
+}
+else {
+    console.log('It is a good deal!');
+}
+
+if (housePrice(5,11,8,70)>1000000){
+    console.log('Julia is paying too much.');
+}
+else {
+    console.log('It is a good deal!');
+}
