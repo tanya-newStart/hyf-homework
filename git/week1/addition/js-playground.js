d05675af2b37992ea0726b61849a575b39900ed9
Tanya Dewland <tanya.dewland@gmail.com>
2024-04-20 10:09:49 +0200

    Create a function that counts vowels in the string

--- git/week1/addition/js-playground.js
+++ git/week1/addition/js-playground.js
@@ -28,3 +28,15 @@ let firstOne = firstWords[randomNumber];
 let secondOne = secondWords[randomNumber];
 let nameLength =firstOne.length+secondOne.length;
 console.log(`The startup: "${firstOne} ${secondOne}" contains ${nameLength} characters.`);
+
+function getCount(str) {
+    let arr = str.split('');
+    let arr_vowel=[];
+    for(i=0;i<arr.length;i++){
+      if (/[aeiou]/.test(arr[i])) {
+        arr_vowel.push(arr[i]);
+      }
+    }
+    return arr_vowel.length;
+  }
+  console.log(getCount("a bacde "));
