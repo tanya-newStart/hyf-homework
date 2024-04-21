bd9120d7704f4f4d89f1ed444ddf68eaeea6f777
Tanya Dewland <tanya.dewland@gmail.com>
2024-04-21 14:48:59 +0200

    Class work javascript week1

---
+++ git/week1/addition/js-classroom.js
@@ -0,0 +1,17 @@
+// Create a variable that is 24 times 55
+let product = 24*55;
+
+// Create a const and set it to be equal to your name
+const myName = "Tanya";
+
+// With javascript console.log the first character in your name
+console.log(myName[0]);
+
+// Create an array with 3 strings, three numbers and three booleans
+const array = ["USA","Mexico", "Denmark", 8, 15, 0, true,true, false];
+
+// console.log the 4. element in the array made above
+console.log(array[3]);
+console.log(typeof array[7]);
+// Optional with javascript console.log the last character in your name.
+console.log(myName[myName.length -1]);
