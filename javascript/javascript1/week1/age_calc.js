40314ebe116141fda5ed3e1071c117604f39470d
Tanya Dewland <tanya.dewland@gmail.com>
2024-04-23 08:40:33 +0200

    Add spaces between operators and commas, rename variables in house_price function and name_generator

--- javascript/javascript1/week1/age_calc.js
+++ javascript/javascript1/week1/age_calc.js
@@ -6,19 +6,19 @@ const age = futureYear - yearOfBirth;
 
 console.log(`You will be ${age} years old in ${futureYear}.`);
 
-
 // A dog age calculator
 const dogYearOfBirth = 2017;
 const dogYearFuture = 2027;
 
-const age_humanYears =dogYearFuture - dogYearOfBirth;
-const dogYear = age_humanYears*7;
+const ageHumanYears = dogYearFuture - dogYearOfBirth;
+const dogYear = ageHumanYears * 7;
 
-let shouldShowResultInDogYears = false;
+const shouldShowResultInDogYears = false;
 
-if (shouldShowResultInDogYears){
-    console.log(`Your dog will be ${dogYear} dog years old in ${dogYearFuture}`);
+if (shouldShowResultInDogYears) {
+  console.log(`Your dog will be ${dogYear} dog years old in ${dogYearFuture}`);
+} else {
+  console.log(
+    `Your dog will be ${ageHumanYears} human years old in ${dogYearFuture}.`
+  );
 }
-else {
-    console.log(`Your dog will be ${age_humanYears} human years old in ${dogYearFuture}.`);
-};
--- javascript/javascript1/week1/name_generator.js
+++ javascript/javascript1/week1/name_generator.js
@@ -1,30 +1,32 @@
-const firstWords =[
-    "Tech",
-    "Labs",
-    "Innovate",
-    "Cloud",
-    "NexGen",
-    "Solutions",
-    "Digital",
-    "Spark",
-    "Genius",
-    "Hub"
+const firstWords = [
+  "Tech",
+  "Labs",
+  "Innovate",
+  "Cloud",
+  "NexGen",
+  "Solutions",
+  "Digital",
+  "Spark",
+  "Genius",
+  "Hub",
 ];
-const secondWords =[
-    "Forge",
-    "Venture",
-    "Code",
-    "Analytics",
-    "Matrix",
-    "Insight",
-    "Byte",
-    "Logic",
-    "Pulse",
-    "Edge"
+const secondWords = [
+  "Forge",
+  "Venture",
+  "Code",
+  "Analytics",
+  "Matrix",
+  "Insight",
+  "Byte",
+  "Logic",
+  "Pulse",
+  "Edge",
 ];
 
-const randomNumber = Math.floor(Math.random() * 10);//random number between 0 and 9
-const firstOne = firstWords[randomNumber];
-const secondOne = secondWords[randomNumber];
-const nameLength =firstOne.length+secondOne.length;
-console.log(`The startup: "${firstOne} ${secondOne}" contains ${nameLength} characters.`);
+const randomNumber = Math.floor(Math.random() * 10); //random number between 0 and 9
+const randomFirstWord = firstWords[randomNumber];
+const randomSecondWord = secondWords[randomNumber];
+const nameLength = randomFirstWord.length + randomSecondWord.length;
+console.log(
+  `The startup: "${randomFirstWord} ${randomSecondWord}" contains ${nameLength} characters.`
+);
