33d255efc0fd1d3f35b536465b1d6fe06733a826
Tanya Dewland <tanya.dewland@gmail.com>
2024-04-28 15:57:40 +0200

    Call function in a loop

--- javascript/javascript1/for_loop.js
+++ javascript/javascript1/for_loop.js
@@ -7,11 +7,15 @@ function logString(stringToLog, numberOfTimesToLog) {
 logString("Happy Sunday!", 4);
 
 // This function emulates sending emails to receipients
+const receipientsArray =
+  "benjamin@gmail.com|peter@gmail.com|hans@gmail.com|ahmad@gmail.com|sana@gmail.com|virgeen@gmail.com|mohammed@gmail.com".split(
+    "|"
+  );
+for (let i = 0; i < receipientsArray.length; i++) {
+  sendEmailTo(receipientsArray[i]);
+}
 function sendEmailTo(recepient) {
   // But really it only logs out a string
-  const receipientsArray = recepient.split("|");
-  for (let i = 0; i < receipientsArray.length; i++) {
-    console.log("email sent to " + receipientsArray[i]);
-  }
+
+  console.log("email sent to " + recepient);
 }
-sendEmailTo("jj|gjg");
