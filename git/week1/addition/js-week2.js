d20dabe706aa5ccfcc498b8e662cb82a75a5a32c
Tanya Dewland <tanya.dewland@gmail.com>
2024-04-29 17:45:09 +0200

    Add try...catch block

--- git/week1/addition/js-week2.js
+++ git/week1/addition/js-week2.js
@@ -1,21 +1,35 @@
-function getFullname(firstname, lastname, useFormalName) {
-  if (firstname === "" || lastname === "") {
-    alert("You did not enter your first and last name");
-  }
-  if (useFormalName) {
-    return "Lord " + firstname + " " + lastname;
-  } else {
-    return firstname + " " + lastname;
+function getFullname(
+  firstname,
+  lastname,
+  useFormalName = false,
+  isFemale = false
+) {
+  try {
+    if (!firstname || !lastname)
+      throw "Both first and last names are required.";
+    if (useFormalName && isFemale) {
+      return "Baroness " + firstname + " " + lastname;
+    }
+    if (useFormalName) {
+      return "Lord " + firstname + " " + lastname;
+    } else {
+      return firstname + " " + lastname;
+    }
+  } catch (error) {
+    return error;
   }
 }
+
 fullname1 = getFullname("Benjamin", "Hughes", true); // returns "Benjamin Hughes"
-fullname2 = getFullname("Tanya", "Dewland"); // add html input for gender?
+fullname2 = getFullname("Tanya", "Dewland", true, true); //need another input of type "checkbox" for gender in html
+fullname3 = getFullname("Quick", false, true);
 console.log(fullname1);
 console.log(fullname2);
+console.log(fullname3);
 
 getFullname("Benjamin", "Hughes", true); // returns "Lord Benjamin Hughes"`
 console.log(getFullname("Benjamin", "Hughes", false)); // returns "Benjamin Hughes"
-//console.log(getFullname("",""));
+console.log(getFullname("", ""));
 
 //Day of the week
 // With todays weekday a tuesday
