162bbdf5fe1990683ad25043a1a95c55f02a9ad4
Tanya Dewland <tanya.dewland@gmail.com>
2024-05-02 11:32:20 +0200

    Create nested array and a single if statement in for loop

--- javascript/javascript1/week2/weather_wear.js
+++ javascript/javascript1/week2/weather_wear.js
@@ -1,21 +1,18 @@
 function smartWear(temp) {
   const clothes = [
-    "shorts and a t-shirt",
-    "jeans and a sweater",
-    "light coat",
-    "winter coat and a winter hat",
+    ["shorts and a t-shirt", [18, 48]],
+    ["jeans and a sweater", [12, 18]],
+    ["light coat", [5, 12]],
+    ["winter coat and a winter hat", [-5, 5]],
   ];
-  if (temp >= 18) {
-    return clothes[0];
-  } else if (temp >= 12) {
-    return clothes[1];
-  } else if (temp >= 5) {
-    return clothes[2];
-  } else if (temp >= -5) {
-    return clothes[3];
-  } else {
-    return "Stay home! It's freezing!";
+  for (let i = 0; i < clothes.length; i++) {
+    const tempRange = clothes[i][1];
+    if (temp >= tempRange[0] && temp < tempRange[1]) {
+      return clothes[i][0];
+    }
   }
+  return `Stay home! It's freezing!`;
 }
-const clothesToWear = smartWear(8);
+
+const clothesToWear = smartWear(12);
 console.log(clothesToWear); // Logs out: "shorts and a t-shirt"
