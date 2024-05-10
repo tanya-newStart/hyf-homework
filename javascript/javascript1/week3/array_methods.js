8784a692eda4b236f179dea4c8bcd77088a22cca
Tanya Dewland <tanya.dewland@gmail.com>
2024-05-10 09:26:27 +0200

    Handle the case when indexOf() returns -1

--- javascript/javascript1/week3/array_methods.js
+++ javascript/javascript1/week3/array_methods.js
@@ -8,6 +8,10 @@ const names = [
   "katrine",
   "Tala",
 ];
-const nameToRemove = "Ahmad";
-names.splice(names.indexOf(nameToRemove), 1); //removes one element at specified index
+const nameToRemove = "Tala";
+const indexOfNameToRemove = names.indexOf(nameToRemove);
+
+if (indexOfNameToRemove >= 0) {
+  names.splice(indexOfNameToRemove, 1); //removes one element at specified index
+}
 console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
