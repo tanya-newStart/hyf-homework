dfb0f935d1bf7e2bf1bd12fdee5037feb82b3379
Tanya Dewland <tanya.dewland@gmail.com>
2024-05-10 09:33:46 +0200

    Add comments

--- javascript/javascript1/week3/travel_time.js
+++ javascript/javascript1/week3/travel_time.js
@@ -1,11 +1,11 @@
 const travelInformation = {
-  speed: 50,
-  destinationDistance: 432,
+  speed: 50, //in km/h
+  destinationDistance: 432, //in km
 };
 function calculateTime(travelObject) {
-  const time = travelObject.destinationDistance / travelObject.speed;
-  const hours = Math.floor(time);
-  const minutes = Math.round((time - hours) * 60);
+  const time = travelObject.destinationDistance / travelObject.speed; // find time it takes to travel the distance in hours
+  const hours = Math.floor(time); //whole hours
+  const minutes = Math.round((time - hours) * 60); //fractional part of an hour converted into minutes
   return `${hours} hours and ${minutes} minutes.`;
 }
 const travelTime = calculateTime(travelInformation);
