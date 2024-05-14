93fe3c347d9b50731eb6cad506bd85d6b5a4408a
Tanya Dewland <tanya.dewland@gmail.com>
2024-05-14 10:39:55 +0200

    Add comments to getCharacterFrequencies function

--- javascript/javascript1/week4/frequency.js
+++ javascript/javascript1/week4/frequency.js
@@ -1,41 +1,12 @@
-//
-//console.log(getCharacterFrequencies('happy'));
-const alphabet = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
 function getCharacterFrequencies(str) {
-  const arr = str.split("");
-  const letterCount = [];
+  const arr = str.split(""); //return array of characters
+  const letterCount = []; //initialize array to store letters and their count
   for (let i = 0; i < arr.length; i++) {
-    const character = arr[i];
+    const character = arr[i]; //current character of arr
     let found = false;
 
     for (let j = 0; j < letterCount.length; j++) {
+      //this loop goes through array of objects and check if the character already there. if it is, the character count increases
       if (letterCount[j].character === character) {
         letterCount[j].count++;
         found = true;
