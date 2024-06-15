25c744bb9e443e733af701c6197b2cdfad819f16
Tanya Dewland <tanya.dewland@gmail.com>
2024-06-15 11:15:27 +0200

    Get random dog image with a breed, get image based on breed

--- javascript/javascript3/week1/dog-breed.js
+++ javascript/javascript3/week1/dog-breed.js
@@ -1,36 +1,101 @@
 document.addEventListener("DOMContentLoaded", domLoaded);
-const image = document.getElementById("dog-breed");
-let loading = true;
+
+const imageContainer = document.getElementById("image-container");
+const userInputContainer = document.getElementById("user-input");
+let dogBreeds = {};
+let randomDogBreed = "";
 
 function domLoaded() {
+  fetchRandomDogImage();
+  fetchDogBreeds();
+}
+
+function fetchRandomDogImage() {
   fetch("https://dog.ceo/api/breeds/image/random")
-    .then((photos) => {
-      return photos.json();
+    .then((response) => response.json())
+    .then((data) => {
+      updateDom(data);
+      randomDogBreed = data.message.split("/")[4];
+      randomDogBreed = randomDogBreed.replace("-", " ");
+      if (randomDogBreed.includes(" ")) {
+        const [breedName, subBreed] = randomDogBreed.split(" ");
+        randomDogBreed = `${
+          subBreed.charAt(0).toUpperCase() + subBreed.slice(1)
+        } ${breedName}`;
+      } else {
+        randomDogBreed =
+          randomDogBreed.charAt(0).toUpperCase() + randomDogBreed.slice(1);
+      }
+      setupAutocomplete(randomDogBreed);
     })
-    .then((data) => updateDom(data))
-    .catch((error) => console.log("Something went wrong", error))
-    .finally(() => (loading = false));
+    .catch((error) => console.log("Something went wrong", error));
+}
 
+function fetchDogBreeds() {
   fetch("https://dog.ceo/api/breeds/list/all")
-    .then((breeds) => {
-      return breeds.json();
+    .then((response) => response.json())
+    .then((data) => {
+      dogBreeds = data.message;
     })
-    .then((data) => updateBreed(data));
+    .catch((error) => console.log("Something went wrong", error));
 }
-function updateDom(photos) {
-  image.innerHTML = "";
+
+function updateDom(photo) {
+  imageContainer.innerHTML = "";
   const dogImage = document.createElement("img");
-  dogImage.src = photos.message;
-  image.appendChild(dogImage);
+  dogImage.src = photo.message;
+  dogImage.alt = "An image of the dog";
+  dogImage.classList.add("dog-image");
+  imageContainer.appendChild(dogImage);
 }
 
-function updateBreed(breeds) {
-  const userInput = document.createElement("input");
+// autocomplete
+function setupAutocomplete(randomDogBreed) {
+  const input = document.createElement("input");
+  input.type = "text";
+  input.placeholder = randomDogBreed;
+  input.setAttribute("list", "breeds");
+
+  const datalist = document.getElementById("breeds");
+
+  Object.keys(dogBreeds).forEach((breed) => {
+    if (dogBreeds[breed].length > 0) {
+      dogBreeds[breed].forEach((subBreed) => {
+        const option = document.createElement("option");
+        option.value = `${subBreed} ${breed}`;
+        datalist.appendChild(option);
+      });
+    } else {
+      const option = document.createElement("option");
+      option.value = breed;
+      datalist.appendChild(option);
+    }
+  });
+  userInputContainer.appendChild(input);
+
   const submitBtn = document.createElement("button");
-
   submitBtn.type = "submit";
-  userInput.type = "text";
-  const dogBreeds = breeds.message;
-  console.log(dogBreeds);
+  userInputContainer.appendChild(submitBtn);
+  submitBtn.textContent = "Submit";
+
+  submitBtn.addEventListener("click", () => {
+    const breed = input.value.toLowerCase().trim();
+    let breedName, subBreed;
+    const breedParts = breed.split(" ");
+    breedName = breedParts[0];
+    subBreed = breedParts[1];
+    if (breedName && subBreed) {
+      fetch(`https://dog.ceo/api/breed/${subBreed}/${breedName}/images/random`)
+        .then((response) => response.json())
+        .then((data) => updateDom(data))
+        .catch((error) => console.log(error));
+    } else if (dogBreeds[breed]) {
+      fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+        .then((response) => response.json())
+        .then((data) => updateDom(data))
+        .catch((error) => console.log(error));
+    } else {
+      alert("Breed not found");
+    }
+  });
 }
-setInterval(domLoaded, 2000);
