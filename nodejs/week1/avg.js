24b380af4a1cf3d0a2acf0f6ffd81f3263155043
Tanya Dewland <tanya.dewland@gmail.com>
2024-07-30 19:58:56 +0200

    Add validation for arguments

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -1,9 +1,25 @@
 let sum = 0;
 const numArgs = process.argv.length - 2;
 
-for (let i = 2; i < process.argv.length; i++) {
-  sum += +process.argv[i];
-}
+if (numArgs === 0) {
+  console.log("Please provide the arguments");
+} else {
+  let notNumber = false;
 
-const avg = sum / numArgs;
-console.log(avg);
+  for (let i = 2; i < process.argv.length; i++) {
+    if (isNaN(process.argv[i])) {
+      notNumber = true;
+      break;
+    }
+  }
+
+  if (notNumber) {
+    console.log("All arguments must be numbers");
+  } else {
+    for (let i = 2; i < process.argv.length; i++) {
+      sum += +process.argv[i];
+    }
+    const avg = sum / numArgs;
+    console.log(avg);
+  }
+}
