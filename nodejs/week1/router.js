0cde05add18c49ef0d88e60f0cc5e815bc362cc7
Tanya Dewland <tanya.dewland@gmail.com>
2024-08-15 09:51:25 +0200

    Add router

---
+++ nodejs/week1/router.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.send("Hello");
+});
+router.get("/:name", (req, res) => {
+  res.send("Hello " + req.params.name);
+});
+
+export default router;
